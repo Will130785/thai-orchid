5a8e00d3c334a30dcb005c8788cb3cc1212b1301
willconstable <will_constable@msn.com>
2020-08-27 11:45:20 +0100

    Add basic layout and carousel functionality

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+    });
+        //Burger animation
+        burger.classList.toggle("toggle");
+    });
+}
+
+navSlide();
+
+new Glide('.options-autoplay-1', {
+    type: "carousel",
+    autoplay: 5000,
+    hoverpause: false,
+    perView: 1
+  }).mount();
